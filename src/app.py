fb9280643215467bd8c64c9b394d2346ae88e148
Matt-bryant21 <14771@coderacademy.edu.au>
2023-12-06 14:16:22 +1100

    added roles and authorization

--- src/app.py
+++ src/app.py
@@ -68,7 +68,7 @@ def seed_db():
     # Seed users
     admin_user = User(
         username="dana_white",
-        password="1234",
+        password=bcrypt.generate_password_hash("1234").decode('utf-8'),
         role="admin"
     )
     db.session.add(admin_user)
@@ -134,6 +134,67 @@ def seed_db():
 def hello():
     return 'Hello'
 
+# register user
+@app.route('/register', methods=['POST'])
+@jwt_required()
+def register():
+    # Check if the current user has the "admin" role
+    current_user = get_jwt_identity()
+    user = User.query.filter_by(username=current_user['username']).first()
+
+    if user.role != 'admin':
+        return jsonify({'message': 'Unauthorized'}), 403
+
+    # Proceed with user registration
+    data = request.get_json()
+
+    username = data.get('username')
+    password = data.get('password')
+    role = data.get('role')
+
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    new_user = User(username=username, password=hashed_password, role=role)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User registered successfully'}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+
+    username = data.get('username')
+    password = data.get('password')
+
+    user = User.query.filter_by(username=username).first()
+
+    if user and bcrypt.check_password_hash(user.password, password):
+        access_token = create_access_token(identity={'username': user.username, 'role': user.role})
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify({'message': 'Invalid username or password'}), 401
+
+
+# delete fighter 
+@app.route('/delete_fighter/<int:fighter_id>', methods=['DELETE'])
+@jwt_required()
+def delete_fighter(fighter_id):
+    current_user = get_jwt_identity()
+    user = User.query.filter_by(username=current_user['username']).first()
+
+    # Check if the user has the required role to delete fighters
+    if user.role == 'admin':  # Adjust this condition based on your roles
+        fighter = Fighter.query.get(fighter_id)
+
+        if fighter:
+            db.session.delete(fighter)
+            db.session.commit()
+            return jsonify({'message': 'Fighter deleted successfully'}), 200
+        else:
+            return jsonify({'message': 'Fighter not found'}), 404
+    else:
+        return jsonify({'message': 'Unauthorized'}), 403
 
 if __name__ == '__main__':
     app.run(debug=True) 
